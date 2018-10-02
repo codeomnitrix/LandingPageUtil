7de76c9042fc6240debaeb521fcedf9f402211f4
Vinit Tiwari <vinitt@Vinits-MacBook-Air.local>
2018-10-02 11:05:40 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Processes Archive to put js, css and other assets in public folder
+// returns html file with updated resources location
+exports.processArchive = function(archivePath, outputPath) {
+
+}
+
+exports.updateOfferUrl = function(pageString, linkClassName, url, identifier) {
+	
+}
