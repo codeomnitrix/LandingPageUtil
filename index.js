4fe14f3a1dabfc721fd60fc8e79c0dc82316ad31
Vinit Tiwari <vintiwari@DELC02WP1BMHTD7.sea.corp.expecn.com>
2018-10-03 13:58:51 +0530

    util files in place

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const extractionUtil = require('./lib/extraction-util');
+const fileHandlerUtil = require('./lib/file-handler-util');
+const textReplacementUtil = require('./lib/text-replacement-util');
 // Processes Archive to put js, css and other assets in public folder
 // returns html file with updated resources location
-exports.processArchive = function(archivePath, outputPath) {
-
+exports.processArchive = function(archivePath, outputPath, tempPath) {
+    
+    extractionUtil.extract(archivePath, tempPath);
+    
 }
 
 exports.updateOfferUrl = function(pageString, linkClassName, url, identifier) {
