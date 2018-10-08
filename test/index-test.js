51c57e88f097ba36ee38fd5002b1c450b04e1515
Vinit Tiwari <vinitt@Vinits-MacBook-Air.local>
2018-10-08 07:20:52 +0530

    test updated

--- test/index-test.js
+++ test/index-test.js
@@ -5,18 +5,20 @@ const rimraf = require('rimraf');
 var landingPageUtil = require("../index");
 
 describe("Landing Page Util test", function() {
+    var filesObj = null;
     it("should be able to process archive", function(done) {
         landingPageUtil.processArchive("/Users/vinitt/Documents/AffTracker/LandingPageService/public/uploads/test.zip", 
         __dirname + "/temp", __dirname + "/temp", function(files) {
             expect(files['js'] != null)
             expect(files['css'] != null)
             expect(files['html'] != null);
+            filesObj = files;
             done();
         })
     });
 
     after(function(done) {
-        rimraf(files['html'][0].substr(0, files['html'][0].lastIndexOf("/")), function(err) {
+        rimraf(filesObj['html'][0].substr(0, filesObj['html'][0].lastIndexOf("/")), function(err) {
             throw err;
         });
         done();
